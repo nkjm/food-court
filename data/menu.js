be5e20975d0d60f2de1da0aa4eb879619b6399a1
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-07-19 10:54:22 +0900

    Update data

--- data/menu.js
+++ data/menu.js
@@ -2,23 +2,15 @@
 
 module.exports = [{
     label: "豚玉",
-    image: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+    image: "https://line-objects-dev.com/waiter/menu/butatama.jpg",
     price: 700
 },{
     label: "豚玉モダン焼き",
-    image: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-    price: 850
-},{
-    label: "いか玉",
-    image: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-    price: 700
-},{
-    label: "いか玉モダン焼き",
-    image: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+    image: "https://line-objects-dev.com/waiter/menu/modern.jpg",
     price: 850
 },{
     label: "焼きそば",
-    image: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+    image: "https://line-objects-dev.com/waiter/menu/yakisoba.jpg",
     price: 800,
     option: {
         size: {
@@ -32,6 +24,6 @@ module.exports = [{
     }
 },{
     label: "生ビール",
-    image: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+    image: "https://line-objects-dev.com/waiter/menu/beer.jpg",
     price: 500
 }]
