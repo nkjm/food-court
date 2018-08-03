3dd1ddcf41866561ff6f82a9f67806e15564e18d
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-08-03 18:05:46 +0900

    Fix

--- skill/escalation.js
+++ skill/escalation.js
@@ -50,13 +50,12 @@ module.exports = class SkillEscalation {
         }
 
         // This is tricky but real sender is not important in this skill so we set bot language as sender_language.
-        let sender_language = context.sender_language;
-        context.sender_language = bot.language;
+        t.lang = bot.language;
 
         let message = await flex.escalation_message({
             sender_id: bot.extract_sender_id(),
             sender_name: sender.displayName,
-            sender_language: sender_language,
+            sender_language: context.sender_language.
             bot_language: bot.language,
             message_text: message_text
         })
